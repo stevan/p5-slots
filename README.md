29109a6400fcb3e276dcc955ad3bb8950ef81456
Stevan Little <stevan.little@gmail.com>
2017-10-16 21:57:37 +0200

    some release engineering

---
+++ README.md
@@ -0,0 +1,13 @@
+# slots
+
+A simple pragma for managing class slots.
+
+[![CPAN version](https://badge.fury.io/pl/slots.svg)](https://metacpan.org/pod/slots)
+
+# Copyright and License
+
+This software is copyright (c) 2017 by Stevan Little.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
--- lib/slots.pm
+++ lib/slots.pm
@@ -1,4 +1,5 @@
 package slots;
+# ABSTRACT: A simple pragma for managing class slots.
 
 use strict;
 use warnings;
@@ -22,5 +23,49 @@ sub import {
 __END__
 
 =pod
+
+=head1 SYNOPSIS
+
+    package Point {
+        use strict;
+        use warnings;
+
+        use parent 'UNIVERSAL::Object';
+        use slots (
+            x => sub { 0 },
+            y => sub { 0 },
+        );
+
+        sub clear {
+            my ($self) = @_;
+            $self->{x} = 0;
+            $self->{y} = 0;
+        }
+    }
+
+    package Point3D {
+        use strict;
+        use warnings;
+
+        use parent 'Point';
+        use slots (
+            z => sub { 0 },
+        );
+
+        sub clear {
+            my ($self) = @_;
+            $self->next::method;
+            $self->{z} = 0;
+        }
+    }
+
+=head1 DESCRIPTION
+
+This is a very simple pragma which takes a set of key/value
+arguments and assigns it to the C<%HAS> package variable of
+the calling class.
+
+This module will also detect superclasses and insure that
+slots are inherited correctly.
 
 =cut
